00b6c166cac9a9b385c235dde986c5ff03861012
Ovidiu <ovidiugiorgi@gmail.com>
2019-11-03 19:01:59 +0200

    Remove non-randomized tests for pop

--- heap.test.js
+++ heap.test.js
@@ -27,7 +27,7 @@ describe("default comparator - minimum", () => {
   });
 
   describe("pop", () => {
-    test("should return null when it's empty", () => {
+    test("should return null when empty", () => {
       heap.push(5);
       heap.push(7);
       heap.pop();
@@ -35,71 +35,8 @@ describe("default comparator - minimum", () => {
       expect(heap.pop()).toBeNull();
     });
 
-    test("should return the minimum element after just one pop", () => {
-      heap.push(2);
-      heap.push(5);
-      heap.push(1);
-      heap.push(10);
-      expect(heap.pop()).toBe(1);
-    });
-
-    test("should return the minimum element after multiple pops", () => {
-      heap.push(12);
-      heap.push(15);
-      heap.push(7);
-      heap.push(11);
-      expect(heap.pop()).toBe(7);
-      expect(heap.pop()).toBe(11);
-      expect(heap.pop()).toBe(12);
-      expect(heap.pop()).toBe(15);
-      heap.push(7);
-      heap.push(5);
-      heap.push(19);
-      expect(heap.pop()).toBe(5);
-      expect(heap.pop()).toBe(7);
-      heap.push(27);
-      expect(heap.pop()).toBe(19);
-      heap.push(25);
-      heap.push(15);
-      expect(heap.pop()).toBe(15);
-      expect(heap.pop()).toBe(25);
-      expect(heap.pop()).toBe(27);
-      heap.push(2);
-      heap.push(10);
-      heap.push(7);
-      heap.push(15);
-      heap.push(16);
-      heap.push(9);
-      heap.push(11);
-      expect(heap.pop()).toBe(2);
-      expect(heap.pop()).toBe(7);
-      expect(heap.pop()).toBe(9);
-      expect(heap.pop()).toBe(10);
-      expect(heap.pop()).toBe(11);
-      expect(heap.pop()).toBe(15);
-      expect(heap.pop()).toBe(16);
-      heap.push(2);
-      heap.push(9);
-      heap.push(5);
-      heap.push(3);
-      expect(heap.pop()).toBe(2);
-      expect(heap.pop()).toBe(3);
-      expect(heap.pop()).toBe(5);
-      expect(heap.pop()).toBe(9);
-      heap.push(8);
-      heap.push(15);
-      heap.push(12);
-      heap.push(15);
-      heap.push(15);
-      expect(heap.pop()).toBe(8);
-      expect(heap.pop()).toBe(12);
-      expect(heap.pop()).toBe(15);
-      expect(heap.pop()).toBe(15);
-      expect(heap.pop()).toBe(15);
-    });
-
     test("should return the minimum elements for randomized stream", () => {
-      let size = 10000;
+      let size = 1000;
       const range = 100000;
       const list = getRandomIntList(size, -range, range);
       list.forEach(i => heap.push(i));
@@ -161,54 +98,14 @@ describe("default comparator - minimum", () => {
 });
 
 describe("custom comparator - maximum", () => {
-  const maxComparator = (a, b) => {
-    if (a === b) return 0;
-    if (a < b) return 1;
-    return -1;
-  };
+  const maxComparator = (a, b) => b - a;
 
   beforeEach(() => {
     heap = new Heap(maxComparator);
   });
 
-  test("should return the maximum element", () => {
-    heap.push(5);
-    heap.push(2);
-    heap.push(10);
-    heap.push(8);
-    expect(heap.peek()).toBe(10);
-    expect(heap.pop()).toBe(10);
-    expect(heap.pop()).toBe(8);
-    expect(heap.pop()).toBe(5);
-    expect(heap.pop()).toBe(2);
-    heap.push(11);
-    heap.push(2);
-    expect(heap.pop()).toBe(11);
-    heap.push(1);
-    expect(heap.pop()).toBe(2);
-    heap.push(5);
-    heap.push(51);
-    heap.push(1000);
-    heap.push(77);
-    expect(heap.pop()).toBe(1000);
-    expect(heap.pop()).toBe(77);
-    expect(heap.pop()).toBe(51);
-    expect(heap.pop()).toBe(5);
-  });
-
-  test("should return the maximum elements for negative ones", () => {
-    heap.push(-5);
-    heap.push(-51);
-    heap.push(-1000);
-    heap.push(-77);
-    expect(heap.pop()).toBe(-5);
-    expect(heap.pop()).toBe(-51);
-    expect(heap.pop()).toBe(-77);
-    expect(heap.pop()).toBe(-1000);
-  });
-
-  test("should return the maximum elements for randomized stream", () => {
-    let size = 10000;
+  test("should return the maximum element for randomized stream", () => {
+    let size = 1000;
     const range = 100000;
     const list = getRandomIntList(size, -range, range);
     list.forEach(i => heap.push(i));
